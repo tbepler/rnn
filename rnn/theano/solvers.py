b3452879dc31c49cefc163c7e1ee904a89dfd2ae
tbepler <tbepler@gmail.com>
2016-05-02 12:13:42 -0400

    updating solvers

--- rnn/theano/solvers.py
+++ rnn/theano/solvers.py
@@ -12,45 +12,68 @@ class GeomDecay(object):
     def __call__(self, lr):
         return lr*self.rate
 
+class Momentum(object):
+    def __init__(self, momentum, weights):
+        self.momentum = momentum
+        if momentum > 0:
+            self.velocity = [theano.shared(w.get_value()*0) for w in weights]
+        else:
+            self.velocity = []
+
+    def __call__(self, deltas):
+        if self.momentum > 0:
+            vel_upd = [self.momentum*v + (1-self.momentum)*d for v,d in zip(self.velocity,deltas)]
+            return vel_upd, zip(self.velocity, vel_upd)
+        return deltas, []
+
 class SGD(object):
-    def __init__(self, lr, mom=0.9, decay=NoDecay()):
-        self.lr = lr
-        self.mom = mom
+    def __init__(self, learning_rate, momentum=0.9, decay=NoDecay()):
+        self.learning_rate = learning_rate
+        self.momentum = momentum
         self.decay = decay
 
-    def compile(self, weights, inputs, err, extras):
-        gws = th.grad(err, weights)
-        vs = [theano.shared(w.get_value()*0, broadcastable=w.broadcastable) for w in weights]
-        lr = th.scalar()
-        #mom = th.constant(self.mom)
-        updates = []
-        for w, gw, v in zip(weights, gws, vs):
-            vnext = self.mom*v + (1-self.mom)*gw
-            #vnext = v + gw
-            updates.append((v, vnext))
-            updates.append((w, w-lr*vnext))
-        return theano.function([lr]+inputs, [err]+extras, updates=updates)
-        
-    def __call__(self, data, weights, inputs, err, extras, max_iters=-1):
-        f = self.compile(weights, inputs, err, extras)
+    def _unscaled_deltas(self, weights, grads):
+        return grads, []
+
+    def _updates(self, weights, grads, learning_rate):
+        delta, updates = self._unscaled_deltas(weights, grads)
+        mom = Momentum(self.momentum, weights)
+        mom_delta, mom_updates = mom(delta)
+        w_upd = [(w, w-learning_rate*md) for w,md in zip(weights, mom_delta)]
+        return updates + mom_updates + w_upd
+
+    def _compile(self, inputs, outputs, weights, grads):
+        learning_rate = th.scalar()
+        updates = self._updates(weights, grads, learning_rate)
+        return theano.function([learning_rate]+inputs, outputs, updates=updates)
+
+    def __call__(self, data, inputs, outputs, weights, grads=None, max_iters=-1):
+        if grads is None:
+            grads = theano.grad(outputs[0], weights)
+        f = self._compile(inputs, outpus, weights, grads)
         n = len(data)
         i = 0
         while i < max_iters or max_iters < 0:
             j = 0.0
             for args in data:
-                ret = f(self.lr, *args)
+                ret = f(self.learning_rate, *args)
                 j += 1
                 yield i+j/n, ret
             i += 1
-            self.lr = self.decay(self.lr)
+            self.learning_rate = self.decay(self.learning_rate)
 
-class RMSprop(object):
-    def __init__(self, lr, rho=0.95, eps=1e-5, mom=0.9, decay=NoDecay()):
-        self.lr = lr
+class RMSprop(SGD):
+    def __init__(self, lr, rho=0.95, eps=1e-5, momentum=0.9, decay=NoDecay()):
+        super(SGD, self).__init__(lr, momentum=momentum, decay=decay)
         self.rho = rho
         self.eps = eps
-        self.mom = mom
         self.decay = decay
+    
+    def _unscaled_deltas(self, weights, grads):
+        history = [theano.shared(w.get_value()*0) for w in weights]
+        hist_upd = [self.rho*h + (1-self.rho)*g*g for h,g in zip(history, grads)]
+        delta = [g/(th.sqrt(h)+eps) for g,h in zip(grads, hist_upd)]
+        return delta, zip(history, hist_upd)
 
     def compile(self, weights, inputs, err, extras):
         gws = th.grad(err, weights)
